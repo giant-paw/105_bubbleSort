59446b80971269842ff6b7981366d30cac08fa44
giant-paw <giantpawbusiness@gmail.com>
2023-03-16 08:38:29 +0700

    penambagan void bubbleSortArray

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -21,6 +21,23 @@ void input() {
     cout << "============================" << endl;
     cout << "Masukkan Element Array" << endl;
     cout << "============================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+    }
+}
+
+void bubbleSortArray() {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main(){
